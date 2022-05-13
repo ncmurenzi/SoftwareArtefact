3a915cef214d8c5d598aaaac2e1b80d8aa43b3eb
ncmurenzi <105379066+ncmurenzi@users.noreply.github.com>
2022-05-13 14:13:57 +0200

    updated app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import pandas as pd
 app = Dash(__name__)
 
 df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+    'https://raw.githubusercontent.com/ncmurenzi/SoftwareArtefact/main/survey-clean_version.csv')
 
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
+fig = px.scatter(df, x="Age", y="Country", color="work_interfere", hover_name="no_employees", title="Country by Age when responding to wether they have interferance in their work",
+                 log_x=True, size_max=80)
+
 
 app.layout = html.Div([
     dcc.Graph(
@@ -21,6 +21,7 @@ app.layout = html.Div([
         figure=fig
     )
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
